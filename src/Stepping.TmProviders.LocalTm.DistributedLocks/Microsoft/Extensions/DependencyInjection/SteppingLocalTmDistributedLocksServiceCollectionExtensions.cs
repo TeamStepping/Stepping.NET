c4c10fd56d8a5cc9c0ef09d792dc24130a6e62bc
Jadyn <jadyn.wong@live.com>
2022-09-04 21:29:21 +0800

    Add `MedallionSteppingDistributedLock`

---
+++ src/Stepping.TmProviders.LocalTm.DistributedLocks/Microsoft/Extensions/DependencyInjection/SteppingLocalTmDistributedLocksServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Medallion.Threading;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class SteppingLocalTmDistributedLocksServiceCollectionExtensions
+{
+    public static IServiceCollection AddLocalTmDistributedLockProvider(
+        this IServiceCollection services, Func<IServiceProvider, IDistributedLockProvider> setupDistributedLockProvider)
+    {
+        services.AddSingleton(setupDistributedLockProvider);
+
+        return services;
+    }
+}
